0e535a1bff2412d0b8ef212d6487cfbdd4e7c69c
Matthew Sherman <msherman0801@gmail.com>
2018-10-20 19:41:13 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,8 +1,8 @@
 def my_select(collection)
   i = 0 
   while i < collection.array 
-    if yield(collection[i]) == true
-      "good job"
+    if yield(collection[i]).even?
+      i
     end
     i += 1
   end
