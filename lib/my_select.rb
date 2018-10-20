7a5ff3308178b0a5c799ef07d5e21329d6508f39
Matthew Sherman <msherman0801@gmail.com>
2018-10-20 19:54:23 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -2,7 +2,7 @@ def my_select(collection)
   i = 0 
   newArray = []
   if collection.length > 0
-    yield(collection[i])
+    if yield(collection[i]).even?
       newArray.push(collection[i])
       i += 1
   else
