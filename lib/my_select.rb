498fd86e03690132255431af49feb85af3ee5def
Matthew Sherman <msherman0801@gmail.com>
2018-10-20 20:10:05 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  i = 0 
+  newArray = []
+  while i < collection.length
+    if yield(collection[i])
+      newArray.push(collection[i])
+    end
+    i += 1
+  end
+  newArray
 end
